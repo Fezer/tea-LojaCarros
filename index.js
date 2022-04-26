ea63284fa04f56063759391854a79532f3ac71e3
Nickolas Ukasinski <nickolas.ukasinski@tuntscorp.com>
2022-04-26 20:33:33 -0300

    test

--- index.js
+++ index.js
@@ -17,8 +17,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(router);
 
-app.listen(process.env.SYSTEM_PORT, () => {
-    console.log("Server running on port ", process.env.SYSTEM_PORT);
-});
-
 module.exports = app;
